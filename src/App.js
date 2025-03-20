eb57fbf408d95165ddd1f888a3c67c56b94af049
Ayden <ajgg1125@gmail.com>
2025-03-20 18:23:53 -0400

    migrated to react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This website is under construction (currently being reconstructed in ReactJS), please sit tight!</h1>
+    </div>
+  );
+}
+
+export default App;
