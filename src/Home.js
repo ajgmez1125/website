f70b2d9c89cfb65f51661b56ea26240210012e93
Ayden <ajgg1125@gmail.com>
2025-03-20 20:38:52 -0400

    v2

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ function Home(){
   return (
     <div>
         <h1>welcome to harpys cool website</h1>
+        <p>this site is still under construction, hang tight while i make it awesomesauce</p>
     </div>
   );
 };
